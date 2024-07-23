10854fbef831d41767bee695cc3416b434a3686c
intelli.hill <intelli.hill@outlook.com>
2024-07-23 20:50:09 +0900

    update: Linkedlist

--- LinkedList/240723/LinkedList.js
+++ LinkedList/240723/LinkedList.js
@@ -1,9 +1,12 @@
 import LinkedListNode from "./LinkedListNode.js";
+import Comparator from "./Comparator.js";
 
 export default class LinkedList {
-  constructor() {
+  constructor(compareFunction) {
     this.head = null;
     this.tail = null;
+
+    this.compare = new Comparator(compareFunction);
   }
 
   prepend (value) {
@@ -23,11 +26,99 @@ export default class LinkedList {
 
     if (!this.head) {
       this.head = newNode;
+      this.tail = newNode;
+      
+      return this;
     }
+
     this.tail.next = newNode;
     this.tail = newNode;
 
     return this;
   }
 
+  insert(value, rawIndex) {
+    const index = rawIndex < 0 ? 0 : rawIndex;
+
+    if (index === 0) {
+      this.prepend(value)
+    } else {
+      let currentNode = this.head;
+      let count = 1;
+      const newNode = new LinkedListNode(value);
+
+      while (currentNode) {
+        if (count === index) break;
+        currentNode = currentNode.next;
+        count += 1;
+      }
+
+      if (currentNode) {
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+      } else {
+        if (this.tail) {
+          this.tail.next = newNode;
+          this.tail = newNode;
+        } else {
+          this.head = newNode;
+          this.tail = newNode;
+        }
+      }
+    }
+    return this;
+  }
+
+  delete(value) {
+
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = null;
+
+    while (this.head && this.compare.equal(this.head.value, value)) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+
+    if (currentNode !== null) {
+      while (currentNode.next) {
+        if (this.compare.equal(currentNode.next.value, value)) {
+          deletedNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+        } else {
+          currentNode = currentNode.next
+        }
+      }
+    }
+    if (this.compare.equal(this.tail.value, value)) {
+      this.tail = currentNode;
+    }
+    return deletedNode;
+  }
+
+  find({ value = undefined, callback = undefined }) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (callback && callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+    
+    return null;
+  }
 }
